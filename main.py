012effc351324334aadcfc6beb259995dc1b0e4b
TGZolars <70763346+TGZolars@users.noreply.github.com>
2021-11-19 10:42:28 +0800

    Update main.py
    new version :3

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
+#  main.py
+#  
+#  Copyright 2021 User TGZolars
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
 # pygame and sys libraries importing
 import pygame as pg
+import random as rand
 import sys
+from environment import Environment
 
 
-# import an integers from one of flat.txt lines
-def parceline(f):
-    return list(map(int, f.readline()[:-1].split()))
+F_WIDTH, F_HEIGHT = int(sys.argv[1]), int(sys.argv[2])
+env = Environment(F_WIDTH, F_HEIGHT)
 
 
 if __name__ == "__main__":
     # flat render scale
-    scale = 20.0
+    scale = 75 / (max(F_WIDTH, F_HEIGHT))
 
-    # importing flat.txt for flatmap
-    filename = "flat.txt" # sys.argv[1]
-    f = open(filename)
-    # width and height of imported flat
-    m, n = parceline(f)
-    flat = list()
-    for i in range(m):
-        flat.append(parceline(f))
-    print(*flat)
-
-    # creating a pygame widow
+    # creating a pygame window
     screen = pg.display.set_mode((1280, 720))
-
-    # cleaner coords import
-    y, x = parceline(f)
 
     # creating a surface for flat render
     view_flat = vx, vy = int(screen.get_width() * 2/3), int(screen.get_height() * 2/3)
@@ -39,24 +48,24 @@ if __name__ == "__main__":
         screen.fill((140, 140, 140))
         sf.fill((110, 110, 110))
         
-        # checking for keyboar, window, mouse inputs or events
+        # checking for keyboard, window, mouse inputs or events
         keys = pg.key.get_pressed()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 exit()
             if event.type == pg.KEYDOWN:
-              
+              pass
               # robot's controls
-              if event.key == pg.K_w:
-                  y -= 1
-              if event.key == pg.K_s:
-                  y += 1
-              if event.key == pg.K_a:
-                  x -= 1
-              if event.key == pg.K_d:
-                  x += 1
-              if event.key == pg.K_SPACE:
-                  flat[x][y] = 0
+              #if event.key == pg.K_w:
+              #    y -= 1
+              #if event.key == pg.K_s:
+              #    y += 1
+              #if event.key == pg.K_a:
+              #    x -= 1
+              #if event.key == pg.K_d:
+              #    x += 1
+              #if event.key == pg.K_SPACE:
+              #    flat[x][y] = 0
 
         # checking for scale changes
         if keys[pg.K_p]:
@@ -65,7 +74,7 @@ if __name__ == "__main__":
             scale -= 0.1
 
         # drawing flat
-        for i, line in enumerate(flat):
+        for i, line in enumerate(env.flat):
             for j, quad in enumerate(line):
                 if not quad:
                     pg.draw.polygon(sf, (255, 255, 255), [[min(vx, i * scale * 5), min(vy, j * scale * 5)],
@@ -87,7 +96,7 @@ if __name__ == "__main__":
                                                           [min(vx, i * scale * 5), min(vy, (j + 1) * scale * 5)]], 1)
 
         # drawing robot
-        pg.draw.circle(sf, (220, 0, 0), [(x + 0.5) * scale * 5, (y + 0.5) * scale * 5], scale * 2.5)
+        pg.draw.circle(sf, (220, 0, 0), [(env.r_x + 0.5) * scale * 5, (env.r_y + 0.5) * scale * 5], scale * 2.5)
 
         pg.draw.polygon(screen, (80, 80, 80), [[0, 0], [vx, 0], [vx, vy], [0, vy]], 1)
 
